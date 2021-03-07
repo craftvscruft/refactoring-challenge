b47a970ba22987f38e23cdfd6e3d7f98c7f86e18
Ray Myers <ray.myers@divvydose.com>
2021-03-07 00:24:18 -0600

    Refactoring

---
+++ 01-trivia-game-html-js/src/questions.js
@@ -0,0 +1,97 @@
+const questions = [
+  {
+    question: "Which language is used for styling web pages?",
+    option: ["CSS", "HTML", "JQuery", "XML"],
+  },
+  {
+    question: "What is HTML?",
+    option: [
+      "Hyper text Markup Language",
+      "Hyper tool Markup Language",
+      "Hyper link Manipulation Language",
+      "Hyper text Manipulation Language",
+    ],
+  },
+  {
+    question: "Which of the following sentence is true about Div tag?",
+    option: [
+      "A block-level element?",
+      "Defines a paragraph",
+      "Creates a line break",
+      "Styles a web page",
+    ],
+  },
+  {
+    question:
+      " Which of the following property sets the shadow for a box element?",
+    option: ["box-shadow", "set-shadow", "canvas-shadow", "shadow"],
+  },
+  {
+    question:
+      "What is the correct HTML for adding a background color of yellow?",
+    option: [
+      '<body style= "background-color: #FFFF00">',
+      '<body style= "background-color: #FFB6C1">',
+      "all of the above",
+      "none of the above",
+    ],
+  },
+  {
+    question: "How to set style of a certain HTML element with id of “quiz”?",
+    option: ["#quiz{}", ".quiz{}", ".quiz()", "Id.quiz{}"],
+  },
+  {
+    question: "How do you add comments in CSS?",
+    option: ["/* comment*/", "<!—comment-->", "// comment", "//comment"],
+  },
+  {
+    question:
+      "Given a margin: 10px 20px 5px 15px \n Where does 5px value will assigned?",
+    option: ["Bottom", "Right", "Left", "Top"],
+  },
+  {
+    question:
+      "Which of the following attributes is used to add link to any element?",
+    option: ["href", "Link", "ref", "li"],
+  },
+  {
+    question: "Why is this keyword not preferred in JavaScript?",
+    option: [
+      "Functions should not access the global object",
+      "Highly memory consuming",
+      "Very inefficient to use",
+      "Functions should access the global objects",
+    ],
+  },
+  {
+    question:
+      "Which method removes the last element from an array and returns that element?",
+    option: ["pop()", "pop()", "pop()", "push()"],
+  },
+  {
+    question: "The object has three object attributes namely ________",
+    option: [
+      "Prototype, class, object’s extensible flag",
+      " Prototype, class, objects’ parameters",
+      " Native object, Classes and Interfaces and Object’s extensible flag",
+      "lClass, parameters, object’s extensible flag",
+    ],
+  },
+  {
+    question: "JavaScript is interpreted by _________",
+    option: ["Client", "Server", "Object", "None of the above"],
+  },
+  {
+    question: 'What is mean by "this" keyword in javascript?',
+    option: [
+      "It refers current object",
+      "It refers previous object",
+      "It is variable which contains value",
+      "None of the above",
+    ],
+  },
+  {
+    question: "A function with no return value is called______",
+    option: ["Procedures", "Method", "Static function", "Dynamic function"],
+  },
+];
--- 01-trivia-game-html-js/src/quiz.js
+++ 01-trivia-game-html-js/src/quiz.js
@@ -1,101 +1,3 @@
-let questions = [
-  {
-    question: "Which language is used for styling web pages?",
-    option: ["CSS", "HTML", "JQuery", "XML"],
-  },
-  {
-    question: "What is HTML?",
-    option: [
-      "Hyper text Markup Language",
-      "Hyper tool Markup Language",
-      "Hyper link Manipulation Language",
-      "Hyper text Manipulation Language",
-    ],
-  },
-  {
-    question: "Which of the following sentence is true about Div tag?",
-    option: [
-      "A block-level element?",
-      "Defines a paragraph",
-      "Creates a line break",
-      "Styles a web page",
-    ],
-  },
-  {
-    question:
-      " Which of the following property sets the shadow for a box element?",
-    option: ["box-shadow", "set-shadow", "canvas-shadow", "shadow"],
-  },
-  {
-    question:
-      "What is the correct HTML for adding a background color of yellow?",
-    option: [
-      '<body style= "background-color: #FFFF00">',
-      '<body style= "background-color: #FFB6C1">',
-      "all of the above",
-      "none of the above",
-    ],
-  },
-  {
-    question: "How to set style of a certain HTML element with id of “quiz”?",
-    option: ["#quiz{}", ".quiz{}", ".quiz()", "Id.quiz{}"],
-  },
-  {
-    question: "How do you add comments in CSS?",
-    option: ["/* comment*/", "<!—comment-->", "// comment", "//comment"],
-  },
-  {
-    question:
-      "Given a margin: 10px 20px 5px 15px \n Where does 5px value will assigned?",
-    option: ["Bottom", "Right", "Left", "Top"],
-  },
-  {
-    question:
-      "Which of the following attributes is used to add link to any element?",
-    option: ["href", "Link", "ref", "li"],
-  },
-  {
-    question: "Why is this keyword not preferred in JavaScript?",
-    option: [
-      "Functions should not access the global object",
-      "Highly memory consuming",
-      "Very inefficient to use",
-      "Functions should access the global objects",
-    ],
-  },
-  {
-    question:
-      "Which method removes the last element from an array and returns that element?",
-    option: ["pop()", "pop()", "pop()", "push()"],
-  },
-  {
-    question: "The object has three object attributes namely ________",
-    option: [
-      "Prototype, class, object’s extensible flag",
-      " Prototype, class, objects’ parameters",
-      " Native object, Classes and Interfaces and Object’s extensible flag",
-      "lClass, parameters, object’s extensible flag",
-    ],
-  },
-  {
-    question: "JavaScript is interpreted by _________",
-    option: ["Client", "Server", "Object", "None of the above"],
-  },
-  {
-    question: 'What is mean by "this" keyword in javascript?',
-    option: [
-      "It refers current object",
-      "It refers previous object",
-      "It is variable which contains value",
-      "None of the above",
-    ],
-  },
-  {
-    question: "A function with no return value is called______",
-    option: ["Procedures", "Method", "Static function", "Dynamic function"],
-  },
-];
-
 const questionContainer = document.querySelector("#question");
 
 const allRadioButtons = Array.from(
@@ -104,16 +6,16 @@ const allRadioButtons = Array.from(
 
 const nextButton = document.querySelector(".next");
 
-let resultContainer = document.getElementById("result");
+const resultContainer = document.getElementById("result");
 
 let currentQuestionIndex;
 let score;
-let body = document.querySelector("body");
+const body = document.querySelector("body");
 
-let messageContainer = document.getElementById("message");
+const messageContainer = document.getElementById("message");
 
 function displayQuestionOptionText(radioButton, text) {
-  let ansContainer = radioButton.nextElementSibling;
+  const ansContainer = radioButton.nextElementSibling;
   ansContainer.textContent = text;
 }
 
@@ -128,7 +30,6 @@ function displayQuestion() {
     displayQuestionOptionText(radioButton, answers[i]);
   });
 }
-
 
 nextButton.addEventListener("click", handleNextQuestion);
 
@@ -163,19 +64,20 @@ function advanceToNextQuestion() {
 }
 
 function handleNextQuestion() {
-  let checkedRadioButton = document.querySelector("input[type=radio]:checked");
+  const checkedRadioButton = document.querySelector(
+    "input[type=radio]:checked"
+  );
   if (!checkedRadioButton) {
     console.log("No answer. TODO: show message.");
     return;
   }
-  let selectedAnswer = checkedRadioButton.value;
+  const selectedAnswer = checkedRadioButton.value;
   const correctAnswer = questions[currentQuestionIndex].option[0];
   updateScore(selectedAnswer, correctAnswer);
   advanceToNextQuestion();
   displayScore(score);
   displayQuestion();
 }
-
 
 restart();
 displayQuestion();
