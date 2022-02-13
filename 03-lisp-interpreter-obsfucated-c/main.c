d671d6895e37343f43b9ce0a21d5525e480abeb0
Ray Myers <ray.myers@divvydose.com>
2022-02-13 16:53:10 -0600

    Remove comma operators

--- 03-lisp-interpreter-obsfucated-c/main.c
+++ 03-lisp-interpreter-obsfucated-c/main.c
@@ -1,14 +1,20 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 char *
-sbrk(),
-  *S(), *j(), *O, *H;
+sbrk(), *j(), *input, *H;
+
+char* eval(char* input);
+
 K, Y, M = 14;
 double
 atof();
+
+void readLine();
+
 Q(_)
 char* _;
 {
@@ -18,7 +24,8 @@ char* _;
   }
   H = _;
   while (V | !(isspace(*H) | *H == ')' || (*H == '(' && H - _))) {
-    V += (*H == '(') - (*H == ')'), H++;
+    V += (*H == '(') - (*H == ')');
+    H++;
   }
   return H - _;
 }
@@ -36,7 +43,9 @@ char* A(char *_)
   while (isspace(*_)) {
     _++;
   }
-  return O = sbrk(199), *O = '(', strcpy(O + 1, _), O;
+  input = sbrk(199);
+  *input = '(', strcpy(input + 1, _);
+  return input;
 }
 char* Z(char* _)
 {
@@ -44,30 +53,30 @@ char* Z(char* _)
 }
 char* c(char* _)
 {
-  return C(S(C(_)));
+  return C(eval(C(_)));
 }
 char* q(char* _)
 {
-  return A(S(C(_)));
+  return A(eval(C(_)));
 }
 char* t(char* _)
 {
-  char* i = S(C(_));
-  return H = S(C(A(_))),
+  char* i = eval(C(_));
+  return H = eval(C(A(_))),
          __builtin___sprintf_chk(
-           O = sbrk(199),
-           0,
-           __builtin_object_size(O = sbrk(199), 2 > 1 ? 1 : 0),
+             input = sbrk(199),
+             0,
+             __builtin_object_size(input = sbrk(199), 2 > 1 ? 1 : 0),
            !strcmp(H, "()") ? "(%s)" : "(%s %s",
-           i,
+             i,
            H + 1)
 
            ,
-         O;
+         input;
 }
-char* F(char* _)
+char* somethingAboutIf(char* in)
 {
-  return S(C(A(!strcmp(S(C(_)), "()") ? A(_) : _)));
+  return eval(C(A(!strcmp(eval(C(in)), "()") ? A(in) : in)));
 }
 L(char * i, char *s)
 {
@@ -75,15 +84,15 @@ L(char * i, char *s)
 }
 char* b(char* _)
 {
-  return L(S(C(_)), S(C(A(_)))) ? "()" : "t";
+  return L(eval(C(_)), eval(C(A(_)))) ? "()" : "t";
 }
 char* R(char* _)
 {
-  return S(C(_));
+  return eval(C(_));
 }
 char* o(char* _)
 {
-  return atof(S(C(_))) < atof(S(C(A(_)))) ? "t" : "()";
+  return atof(eval(C(_))) < atof(eval(C(A(_)))) ? "t" : "()";
 }
 char* f(char* _)
 {
@@ -93,7 +102,7 @@ char* f(char* _)
            0,
            __builtin_object_size(i = sbrk(199), 2 > 1 ? 1 : 0),
            "%lf",
-           atof(S(C(_))) + atof(S(C(A(_))))),
+           atof(eval(C(_))) + atof(eval(C(A(_))))),
          i;
 }
 char* g(char* _)
@@ -104,7 +113,7 @@ char* g(char* _)
            0,
            __builtin_object_size(i = sbrk(199), 2 > 1 ? 1 : 0),
            "%lf",
-           atof(S(C(_))) - atof(S(C(A(_))))),
+           atof(eval(C(_))) - atof(eval(C(A(_))))),
          i;
 }
 char* h(char* _)
@@ -115,53 +124,65 @@ char* h(char* _)
            0,
            __builtin_object_size(i = sbrk(199), 2 > 1 ? 1 : 0),
            "%lf",
-           atof(S(C(_))) * atof(S(C(A(_))))),
+           atof(eval(C(_))) * atof(eval(C(A(_))))),
          i;
 }
 char* r[4][2] = { "function", (char*)R, "quote", (char*)C,
                   "lambda",   (char*)Z, "defun", (char*)j };
 char* j(char* _)
 {
-  return r[M][1] = A(_), *r[M++] = C(_);
+  r[M][1] = A(_);
+  return *r[M++] = C(_);
 }
-char * not [99][2] = { "if",     (char*)F, "equal",  (char*)b, "<",
-                       (char*)o, "+",      (char*)f, "-",      (char*)g,
-                       "*",      (char*)h, "car",    (char*)c, "cdr",
-                       (char*)q, "cons",   (char*)t, "t",      "t" };
-char* S(char* _)
+char * not [99][2] = {"if", (char*)somethingAboutIf, "equal", (char*)b, "<",
+                      (char*)o, "+", (char*)f, "-", (char*)g,
+                      "*", (char*)h, "car", (char*)c, "cdr",
+                      (char*)q, "cons", (char*)t, "t", "t" };
+char* eval(char* input)
 {
   int Li, s;
   char* u;
-  if (isdigit(*_) | !strcmp(_, "()")) {
-    return _;
+  bool isSelfEvaluating = isdigit(*input) | !strcmp(input, "()");
+  if (isSelfEvaluating) {
+    return input;
   }
   for (Y = M; Y--;) {
-    if (!strcmp(_, *r[Y])) {
+    if (!strcmp(input, *r[Y])) {
       return r[Y][1];
     }
   }
-  u = S(C(_)), _ = A(_);
+  u = eval(C(input)), input = A(input);
   if (*u - '(') {
-    return (*((char* (*)())u))(_);
+    return (*((char* (*)())u))(input);
   }
   s = Li = M;
-  while (!!strcmp(_, "()")) {
-    r[M][1] = S(C(_)), *r[M++] = "", _ = A(_);
+  while (!!strcmp(input, "()")) {
+    r[M][1] = eval(C(input));
+    *r[M++] = "";
+    input = A(input);
   }
-  O = C(u);
-  while (!!strcmp(O, "()")) {
-    *r[Li++] = C(O), O = A(O);
+  input = C(u);
+  while (!!strcmp(input, "()")) {
+    *r[Li++] = C(input), input = A(input);
   }
-  return O = S(C(A(u))), M = s, O;
+  input = eval(C(A(u)));
+  M = s;
+  return input;
 }
 int main()
 {
-  H = O = sbrk(199), Y = 0;
+  // Read Eval Print Loop
+  while (1) {
+    readLine();
+    char *result = eval(input);
+    *H = 0;
+    puts(result);
+  }
+}
+
+void readLine() {
+  H = input = sbrk(199), Y = 0;
   while (Y | !isspace(K = getchar())) {
     K == EOF ? exit(0) : 0, Y += (K == '(') - (K == ')'), *H++ = K;
-  }
-  *H = 0, puts(S(O)), main();
-  {
-    printf("XLISP 4.0\n");
   }
 }
