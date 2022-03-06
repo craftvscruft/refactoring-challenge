e297296e80e5d8691177d6fe9e4085b310c3015c
Ray Myers <ray.myers@divvydose.com>
2022-03-06 14:45:14 -0600

    Add sad path tests for TripService

--- 04-trip-service-java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ 04-trip-service-java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,6 +1,8 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,11 +10,19 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TripServiceTest {
+    private TripService subject;
+    private FakeTripDAO tripDAO;
+    private FakeUserSession session;
+
+    @BeforeEach
+    public void setup() {
+        subject = new TripService();
+        tripDAO = new FakeTripDAO();
+        session = new FakeUserSession();
+    }
+
 	@Test
     public void findUserTripsWhenLoggedInUserIsFriend() {
-        TripService subject = new TripService();
-        FakeTripDAO tripDAO = new FakeTripDAO();
-        FakeUserSession session = new FakeUserSession();
         session.loggedUser = new User();
         User user = new User();
         user.addFriend(session.getLoggedUser());
@@ -24,5 +34,28 @@ public class TripServiceTest {
         assertEquals(1, result.size());
         assertSame(trip, result.get(0));
         assertSame(user, tripDAO.passedUser);
+    }
+
+    @Test
+    public void findNoUserTripsWhenLoggedInUserIsNotFriend() {
+        session.loggedUser = new User();
+        User user = new User();
+        Trip trip = new Trip();
+        tripDAO.foundTrips.add(trip);
+
+        List<Trip> result = subject.getTripsByUser(user, session, tripDAO);
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void throwsWhenNoLoggedInUser() {
+        User user = new User();
+        Trip trip = new Trip();
+        tripDAO.foundTrips.add(trip);
+        
+        assertThrows(UserNotLoggedInException.class, () -> {
+            subject.getTripsByUser(user, session, tripDAO);
+        });
     }
 }
